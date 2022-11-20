a57befd053b4ddebef04a491f34be23f1fee480c
Gary Jennings <garysjennings@gmail.com>
2022-11-20 11:47:40 -0700

    added temp permissions for test functions

--- api/canary/functions/apitest/config.js
+++ api/canary/functions/apitest/config.js
@@ -4,5 +4,13 @@ module.exports.config = {
         URL: '{@output.coffeeApiprod.ApiUrl}',
         USERPOOL_ID: '{@output.coffeeApiprod.UserPoolId}',
         USERPOOL_CLIENT_ID: '{@output.coffeeApiprod.UserPoolClientId}'
-    }
+    },
+    // this * permission is for testing, will lock down
+    permissions: [
+        {
+            Effect: 'Allow',
+            Action: '*',
+            Resource: '*'
+        }
+    ]
 }
--- api/tests/functions/apitest/config.js
+++ api/tests/functions/apitest/config.js
@@ -1,25 +1,15 @@
 module.exports.config = {
-    // url: {
-    //     method: 'GET',
-    //     path: '/blue'
-    // }
-    // alarm: {
-    //     threshold: 1,
-    //     period: 10,
-    //     evaluationPeriod: 2,
-    //     snsTopic: `arn:aws:sns:us-east-1:251256923172:ChatOpsTopic`
-    // }
-    // dashboard: {
-    //     invocationAlarm: 1,
-    //     invocationGoal: 30,
-    //     errorAlarm: 5,
-    //     errorGoal: 0,
-    //     durationAlarm: 10,
-    //     durationGoal: 2
-    // }
     env: {
         URL: '{@output.coffeeApiqa.ApiUrl}',
         USERPOOL_ID: '{@output.coffeeApiqa.UserPoolId}',
         USERPOOL_CLIENT_ID: '{@output.coffeeApiqa.UserPoolClientId}'
-    }
+    },
+    // this * permission is for testing, will lock down
+    permissions: [
+        {
+            Effect: 'Allow',
+            Action: '*',
+            Resource: '*'
+        }
+    ]
 }
